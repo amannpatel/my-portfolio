dc81ea4573a717da2666c42cae82fad233608598
Aman Patel <aman17626@gmail.com>
2024-02-14 22:31:23 +0530

    Setup packages and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useEffect } from "react";
-import Aos from "aos";
-
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    Aos.init();
-  }, []);
-
-  return <h1 className="text-indigo-600 text-2xl font-[500]">Hello world</h1>;
+  return <h1>Hello World</h1>;
 }
 
 export default App;
