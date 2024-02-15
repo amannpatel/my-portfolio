4105ba26d55caf6c3b0596f2e4bfbdf118c011dc
Aman Patel <aman17626@gmail.com>
2024-02-15 12:11:54 +0530

    Update package.json and Hero component

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import heroImg from "../../assets/images/hero.svg";
-// import CountUp from "react-countup/build/CountUp";
+import HeroImg from "../../assets/images/hero.png";
+import CountUp from "react-countup";
 
 const Hero = () => {
   return (
@@ -16,8 +16,107 @@ const Hero = () => {
             >
               Hello Welcome
             </h5>
+            <h1
+              data-aos="fade-right"
+              data-aos-duration="1500"
+              className="text-headingColor font-[800] text-[1.8rem] sm:text-[40px] leading-[35px] sm:leading-[46px] mt-5"
+            >
+              I'm Aman Patel <br />
+              Software Developer
+            </h1>
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1800"
+              data-aos-delay="200"
+              className="flex items-center gap-6 mt-7"
+            >
+              <a href="#contact">
+                <button className="bg-primaryColor text-white font-[500] flex items-center gap-2 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px]">
+                  <i class="ri-mail-line"></i> Hire me
+                </button>
+              </a>
+              <a
+                href="#portfolio"
+                className="text-smallTextColor font-[600] text-[16px] border-b border-solid border-smallTextColor"
+              >
+                See portfolio
+              </a>
+            </div>
+            <p
+              data-aos="fade-left"
+              data-aos-duration="1500"
+              className="flex gap-2 text-headingColor mt-12 font-[500] text-[15px] leading-7 sm:pl-14 sm:pr-10"
+            >
+              <span>
+                <i class="ri-apps-2-line"></i>
+              </span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Incidunt, provident aut iusto accusamus assumenda voluptatem alias
+              quibusdam suscipit! Suscipit, quas.
+            </p>
+            <div className="flex items-center gap-9 mt-14">
+              <span className="text-smallTextColor text-[15px] font-[600]">
+                Connect with me:
+              </span>
+              <span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.linkedin.com/in/amanpatell/"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-linkedin-box-fill"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/amannpatel"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-github-fill"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://leetcode.com/amannpatell/"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-terminal-line"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.instagram.com/amann.kabir/"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-instagram-line"></i>
+                </a>
+              </span>
+            </div>
           </div>
           {/* Hero left content ends */}
+          {/* Hero Image */}
+          <div className="basis-1/3 mt-10 sm:mt-0">
+            <figure className="flex items-center justify-center">
+              <img src={HeroImg} alt="Hero" />
+            </figure>
+          </div>
+          {/* Hero Image end */}
+          {/* Hero content right */}
+          <div className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={6} duration={2} suffix="+" />
+              </h2>
+            </div>
+          </div>
+          {/* Hero content right end */}
         </div>
       </div>
     </section>
